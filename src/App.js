d196003208d23cf240b3771ea3dcb1586b14dad0
Cyriac-Ernest <103205781+cyriac9966@users.noreply.github.com>
2025-11-30 23:13:01 +0100

    Implement routing with BrowserRouter and Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ShoppingListsRoute from "./routes/ShoppingListsRoute";
 import ShoppingListDetailRoute from "./routes/ShoppingListDetailRoute";
+import "./styles/shoppingListDetail.css";
 
 function App() {
-    return <ShoppingListDetailRoute />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Úvodní obrazovka – přehled seznamů */}
+                <Route path="/" element={<ShoppingListsRoute />} />
+
+                {/* Detail jednoho seznamu */}
+                <Route path="/shopping-list/:id" element={<ShoppingListDetailRoute />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
